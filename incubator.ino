25e2e586ec526f53cc8a831dd35a4a667e3524c2
quantenschaum <al@louisenhof2.de>
2018-04-02 15:32:38 +0200

    added comments, cleanup
    no H alarm if H control is off

--- incubator.ino
+++ incubator.ino
@@ -1,49 +1,40 @@
 #include <avr/wdt.h>
 #include <SoftwareServo.h>
-//#include <Streaming.h>
-//#include <OneWire.h>
-//#include <DallasTemperature.h>
-//#include <MemoryFree.h>
 #include <LiquidCrystal.h>
 #include <DHT.h>
 #include <EEPROM.h>
 
 // stty -F /dev/ttyACM0 115200 cs8 cread clocal -hupcl time 30 && tee incubator.log </dev/ttyACM0
 
-#define WDT_TIMEOUT WDTO_8S
-//#define SERIAL_LOGGING
-#define DHTPIN 3
-//#define ONE_WIRE 3
-#define T_OFFSET 0.9
-#define FAN_PIN 2
-#define FAN_THRES 500
-#define BEEPER A2
-#define BRIGHTNESS 10
-#define HEATER A1
-#define DELAY 2000
-#define TS_ADDR 0
-#define HS_ADDR 4
-#define HC_ADDR 8
-#define TI_RESET 1
-#define HI_RESET 5
-#define ALARM_T 2
-#define ALARM_H 8
-#define H_AUTO_THRES 3
-#define H_AUTO_COUNT 200
-#define HWAT 0.25 // holt winters parameters for temperature
+#define WDT_TIMEOUT WDTO_8S // if defined, enable hardware watchdog
+#define DHTPIN 3 // data pin of the DHT T/H sensor
+#define T_OFFSET 0.9 // temperature sensor offset
+#define FAN_PIN 2 // fan tacho signal pin
+#define FAN_THRES 500 // fan alarm threshold
+#define BEEPER A2 // pin where beeper is attached
+#define BRIGHTNESS 10 // display brightness pin
+#define HEATER A1 // heater MOSFET pin
+#define DELAY 2000 // loop delay in ms
+#define TS_ADDR 0 // temperature set point EEPROM address
+#define HS_ADDR 4 // humidity set point EEPROM address
+#define HC_ADDR 8 // humiditiy control mode EEPROM address
+#define TI_RESET 1 // integral reset threshold, set integral to 0 when T error greater than this
+#define HI_RESET 5 // integral reset threshold, set integral to 0 when H error greater than this
+#define ALARM_T 2 // temperature alarm threshold, alert if T error greater than this
+#define ALARM_H 8 // humidity alarm threshold, alert if H error greater than this
+#define H_AUTO_THRES 3 // disable vent control in auto mode if H error < this
+#define H_AUTO_COUNT 200 // disable for n cycles
+#define HWAT 0.25 // holt winters parameters for temperature smoothing
 #define HWBT 0.2
-#define HWAH 0.7 // holt winters parameters for humidity
+#define HWAH 0.7 // holt winters parameters for humidity smoothing
 #define HWBH 0.5
 #define A 0.005 // long average parameter
 #define VENTCLOSED 80 // consider vent closed if under this angle
 #define VENTOPENMS  480000L // open vent if closed longer
-#define VENTRESETMS 120000L // reset vent after this time (> VENTOPENMS)
+#define VENTRESETMS 600000L // reset vent after this time (>VENTOPENMS!)
 
 SoftwareServo vent;
-//OneWire oneWire(ONE_WIRE);
-//DallasTemperature sensors(&oneWire);
 DHT dht(DHTPIN, DHT22);
-//LiquidCrystal lcd(2,3,4,5,6,7);
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 #define RIGHT 16
@@ -139,14 +130,14 @@ void beep(unsigned long f, unsigned long l) {
   pinMode(BEEPER, INPUT);
 }
 
-float Ts, Hs;
-byte Hcontrol;
-byte Ts_changed, Hs_changed;
-float ET, dETdt, IETdt;
-float EH, dEHdt, IEHdt;
-float Tavg = NAN, Tvar;
-float Havg = NAN, Hvar;
-float Hpower, Hduty;
+float Ts, Hs; // set points
+byte Hcontrol; // H control mode
+byte Ts_changed, Hs_changed; // setpoint changed flags
+float ET, dETdt, IETdt; // prop/diff/integ terms for T
+float EH, dEHdt, IEHdt; // prop/diff/integ terms for H
+float Tavg = NAN, Tvar; // T average
+float Havg = NAN, Hvar; // average
+float Hpower, Hduty; // heater current power, average duty cycle
 unsigned long t0, Hon, talarm, tventclosed;
 byte c, displayMode;
 byte key, bri = 255, alarm;
@@ -155,16 +146,13 @@ void setup() {
 #if defined(WDT_TIMEOUT)
   wdt_enable(WDT_TIMEOUT);
 #endif
-#if defined(SERIAL_LOGGING)
-  Serial.begin(115200);
-#endif
   pinMode(HEATER, OUTPUT);
   heater(0);
   pinMode(BRIGHTNESS, OUTPUT);
   analogWrite(BRIGHTNESS, bri = 255);
   lcd.begin(16, 2);
   lcd.noCursor();
-  lcd.print("Incubator 0.5");
+  lcd.print("Incubator 0.6");
   lcd.setCursor(0, 1);
   lcd.print(__DATE__);
   dht.begin();
@@ -235,25 +223,26 @@ void loop() {
     int fanrpm = fancount * 60 / dts;
     fancount = 0;
 
+    // temperature Holt-Winters smoothing
     float E0 = ET;
-    ET = HWAT * (T - Ts) + (1 - HWAT) * (ET + dETdt * dts); // double exp smoothing (holt winters)
-    dETdt = HWBT * (ET - E0) / dts + (1 - HWBT) * dETdt;
-    IETdt += ET * dts;
-    if (abs(ET) > TI_RESET)
+    ET = HWAT * (T - Ts) + (1 - HWAT) * (ET + dETdt * dts); // smoothed T error (deviation from set point)
+    dETdt = HWBT * (ET - E0) / dts + (1 - HWBT) * dETdt; // smoothed derivative
+    IETdt += ET * dts; // integral of error
+    if (abs(ET) > TI_RESET) // reset integral on big deviation
       IETdt = 0;
-    float pidT = 1.1765 * (ET + 0.010526 * IETdt + 23.75 * dETdt);
+    float pidT = 1.1765 * (ET + 0.010526 * IETdt + 23.75 * dETdt); // PID value, adjust coefficients to tune
     Hpower = fanrpm > FAN_THRES ? max(0, min(1, -pidT)) : 0;
     heater(Hpower > 0.1);
     Hon = millis();
 
+    // humidity Holt-Winters smoothing
     E0 = EH;
-    EH = HWAH * (H - Hs) + (1 - HWAH) * (EH + dEHdt * dts); // double exp smoothing (holt winters)
-    dEHdt = HWBH * (EH - E0) / dts + (1 - HWBH) * dEHdt;
-    IEHdt += EH * dts;
-    if (abs(EH) > HI_RESET)
+    EH = HWAH * (H - Hs) + (1 - HWAH) * (EH + dEHdt * dts); // smoothed H error (deviation from set point)
+    dEHdt = HWBH * (EH - E0) / dts + (1 - HWBH) * dEHdt; // smoothed derivative
+    IEHdt += EH * dts; // integral of error
+    if (abs(EH) > HI_RESET) // reset integral on big deviation
       IEHdt = 0;
-    float pidH = 0.1176 * (EH + 0.09091 * IEHdt + 2.75 * dEHdt);
-    //    pidH = max(0, min(1, round(pidH * 10) / 10.0)); // discretize with Hsteps step to avoid mini adjustmnts
+    float pidH = 0.1176 * (EH + 0.09091 * IEHdt + 2.75 * dEHdt); // PID value, adjust coefficients to tune
     vent.write(pidH * 180);
 
     boolean ventclosed0 = ventclosed;
@@ -282,6 +271,7 @@ void loop() {
       }
     }
 
+    // long term averages
     Tavg = A * T + (1 - A) * (isnan(Tavg) ? T : Tavg);
     Tvar = A * pow(T - Tavg, 2) + (1 - A) * (isnan(Tvar) ? 0 : Tvar);
     float Tstd = sqrt(Tvar);
@@ -409,7 +399,7 @@ void loop() {
       alarm &= ~1;
     }
 
-    if (abs(EH) > ALARM_H) {
+    if (Hcontrol && abs(EH) > ALARM_H) {
       alarm |= 2;
     } else {
       alarm &= ~2;
@@ -454,75 +444,6 @@ void loop() {
       alarm = 0;
       talarm = 0;
     }
-
-#if defined(SERIAL_LOGGING)
-    //    Serial.print("free=");    Serial.println(freeMemory());
-    Serial.print("up=");
-    Serial.println(t1 / 1000);
-    Serial.print("dt=");
-    Serial.println(dts, 4);
-    Serial.println();
-
-    Serial.print("Ts=");
-    Serial.println(Ts, 4);
-    Serial.print("Tr=");
-    Serial.println(T, 4);
-    Serial.print("T=");
-    Serial.println(Ts + ET, 4);
-    Serial.print("ET=");
-    Serial.println(ET, 4);
-    Serial.print("dETdt=");
-    Serial.println(dETdt, 4);
-    Serial.print("IETdt=");
-    Serial.println(IETdt, 4);
-    Serial.print("Tavg=");
-    Serial.println(Tavg, 4);
-    Serial.print("Tstd=");
-    Serial.println(Tstd, 4);
-    Serial.print("pidT=");
-    Serial.println(pidT, 4);
-    Serial.print("Ht=");
-    Serial.println(Hpower, 4);
-    Serial.print("Hduty=");
-    Serial.println(Hduty, 4);
-    Serial.println();
-
-    Serial.print("Hs=");
-    Serial.println(Hs, 4);
-    Serial.print("Hcontrol=");
-    Serial.println(Hcontrol);
-    Serial.print("Hr=");
-    Serial.println(H, 4);
-    Serial.print("H=");
-    Serial.println(Hs + EH, 4);
-    Serial.print("EH=");
-    Serial.println(EH, 4);
-    Serial.print("dEHdt=");
-    Serial.println(dEHdt, 4);
-    Serial.print("IEHdt=");
-    Serial.println(IEHdt, 4);
-    Serial.print("Havg=");
-    Serial.println(Havg, 4);
-    Serial.print("Hstd=");
-    Serial.println(Hstd, 4);
-    Serial.print("pidH=");
-    Serial.println(pidH, 4);
-    Serial.print("V=");
-    Serial.println(vent.read() / 180.0, 4);
-    Serial.print("Vdeg=");
-    Serial.println(vent.read());
-    Serial.print("Fan=");
-    Serial.println(fanrpm);
-    Serial.print("vent=");
-    Serial.println(vent.read());
-    Serial.print("vent closed=");
-    Serial.println(ventclosed);
-    Serial.print("vent closed since=");
-    Serial.println(millis() - tventclosed);
-
-
-    Serial.println();
-#endif
 
     if (key) {
       delay(500);
